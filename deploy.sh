ad75eea9c9390ef49e7857b2ced9ba96e5de8838
Josh Long <josh@joshlong.com>
2023-12-03 16:22:42 -0800

    releasing 0.0.12

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 START_DIR=$(cd `dirname $0` && pwd )
 echo "$START_DIR"
+#export BW_SESSION=$(bw unlock --raw)
+function fail(){
+  echo "could not build"
+  exit
+}
 
 echo "this script will prompt you for the GPG passphrase"
 export GPG_TTY=$(tty)
@@ -14,7 +19,16 @@ RELEASE_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdou
 echo "the release version is $RELEASE_VERSION "
 echo "deploying..."
 mvn versions:commit                           # accept the release version
-mvn -DskipTests=true -P publish clean deploy  # deploy to maven central
+
+KEY_LOCAL_PATH=secret-backup.gpg
+KEY_INSTALL_PATH=$HOME/.gnupg/private-keys-v1.d/${KEY_LOCAL_PATH}
+
+echo $GPG_KEY | base64 -d  > $KEY_LOCAL_PATH
+mv ${KEY_LOCAL_PATH} "${KEY_INSTALL_PATH}"
+
+
+mvn -X -DskipTests=true -P publish clean deploy   || fail
+
 git commit -am "releasing ${RELEASE_VERSION}" # release the main version
 TAG_NAME=v${RELEASE_VERSION}
 git tag -a $TAG_NAME -m "release tag ${TAG_NAME}"
@@ -28,3 +42,5 @@ mvn versions:commit
 SNAPSHOT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 git commit -am "moving to $SNAPSHOT_VERSION"
 git push
+
+rm "${KEY_INSTALL_PATH}"
